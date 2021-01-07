77019dee799af69b3835b31b4179247c305a8f1b
thomveen <70706635+thomveen@users.noreply.github.com>
2021-01-07 13:58:36 +0100

    Add files via upload

---
+++ ui/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+import { AuthService } from '@/app/core/auth/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  constructor(
+    router: Router,
+    translate: TranslateService,
+    private $auth: AuthService,
+  ) {
+    // this array needs to be updated each time a new translation is added
+    const languages = ['en', 'de', 'fr', 'pl', 'cs', 'ru', 'zh-CN', 'zh-TW', 'hu', 'ja', 'es', 'nl', 'tr', 'it', 'bg', 'sv', 'no', 'sl', 'pt-BR', 'pt', 'id', 'ca', 'ko'];
+
+    console.log('Browser Culture Lang:', translate.getBrowserCultureLang());
+    console.log('Browser Lang:', translate.getBrowserLang());
+
+    const browserLang = languages.find(x => x === translate.getBrowserLang() || x === translate.getBrowserCultureLang());
+
+    for (const lang of languages) {
+      translate.setTranslation(lang, require('../i18n/' + lang + '.json'));
+    }
+
+    if (browserLang) {
+      translate.use(browserLang);
+    } else {
+      translate.setDefaultLang('en');
+    }
+
+    // ensure the menu closes when we navigate
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const dropdownMenu = window.document.querySelector('#navbarSupportedContent');
+        if (dropdownMenu) {
+          dropdownMenu.classList.remove('show');
+        }
+      }
+    });
+  }
+}
---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,77 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { DragulaModule } from 'ng2-dragula';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './core/auth/auth.module';
+import { RestartModule } from './modules/restart/restart.module';
+import { StatusModule } from './modules/status/status.module';
+
+import { onMonacoLoad } from './core/monaco-editor.service';
+import { supportedLocales } from './core/locales';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+  ],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    ToastrModule.forRoot({
+      autoDismiss: true,
+      newestOnTop: false,
+      closeButton: true,
+      maxOpened: 2,
+      positionClass: 'toast-bottom-right',
+    }),
+    NgbModule,
+    MonacoEditorModule.forRoot({
+      defaultOptions: {
+        scrollBeyondLastLine: false,
+        quickSuggestions: true,
+        parameterHints: true,
+        formatOnType: true,
+        formatOnPaste: true,
+        folding: true,
+        minimap: {
+          enabled: false,
+        },
+      },
+      onMonacoLoad,
+    }),
+    DragulaModule.forRoot(),
+    CoreModule,
+    AuthModule,
+    StatusModule,
+    RestartModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useFactory: (translate: TranslateService) => {
+        if (translate.currentLang in supportedLocales) {
+          return supportedLocales[translate.currentLang];
+        } else {
+          return 'en';
+        }
+      },
+      deps: [TranslateService],
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
