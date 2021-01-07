10f8c6664a1079919399a457a3de456ec4dc55ae
thomveen <70706635+thomveen@users.noreply.github.com>
2021-01-07 14:01:24 +0100

    Add files via upload

---
+++ ui/src/app/core/core.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { Bootstrap4FrameworkModule } from '@oznu/ngx-bs4-jsonform';
+
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { ConvertTempPipe } from './pipes/convert-temp.pipe';
+import { ReplacePipe } from './pipes/replace.pipe';
+import { ExternalLinkIconPipe } from './pipes/external-link-icon.pipe';
+import { HrefTargetBlankDirective } from './directives/href-target-blank.directive';
+import { LongClickDirective } from './directives/longclick.directive';
+import { BackupRestoreComponent } from './backup-restore/backup-restore.component';
+import { ScheduledBackupsComponent } from './backup-restore/scheduled-backups/scheduled-backups.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
+import { SchemaFormComponent } from './components/schema-form/schema-form.component';
+
+@NgModule({
+  entryComponents: [
+    BackupRestoreComponent,
+    ScheduledBackupsComponent,
+    ConfirmComponent,
+  ],
+  declarations: [
+    SpinnerComponent,
+    SchemaFormComponent,
+    ConvertTempPipe,
+    ReplacePipe,
+    ExternalLinkIconPipe,
+    HrefTargetBlankDirective,
+    LongClickDirective,
+    BackupRestoreComponent,
+    ScheduledBackupsComponent,
+    ConfirmComponent,
+  ],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    Bootstrap4FrameworkModule,
+  ],
+  providers: [],
+  exports: [
+    SpinnerComponent,
+    SchemaFormComponent,
+    ConvertTempPipe,
+    ReplacePipe,
+    ExternalLinkIconPipe,
+    HrefTargetBlankDirective,
+    LongClickDirective,
+  ],
+})
+export class CoreModule { }
---
+++ ui/src/app/core/monaco-editor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+const readyEvent = new Subject();
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MonacoEditorService {
+  public readyEvent: Subject<any>;
+
+  constructor() {
+    this.readyEvent = readyEvent;
+  }
+}
+
+export function onMonacoLoad() {
+  readyEvent.next();
+}
